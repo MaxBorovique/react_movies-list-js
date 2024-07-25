b9746ac0785af87b1bccaaf4096bf368969c9287
Maksym <max.borovique@gmail.com>
2024-07-25 17:20:08 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.scss';
 // import moviesFromServer from './api/movies.json';
 
 export const App = () => (
+  
   <div className="page">
     <div className="page-content">
       <div className="movies">
